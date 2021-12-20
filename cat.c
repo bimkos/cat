6ceb8b99e1a3d96734cc87de721093e3f1a06daa
Dothelm <49758889+bimkos@users.noreply.github.com>
2021-12-21 01:32:05 +0300

    Update cat.c

--- cat.c
+++ cat.c
@@ -2,28 +2,28 @@
 #include <inttypes.h>
 #include <string.h>
 
-void cat(char name[1024]) {
-	FILE *file;
-	unsigned char buffer[1024]; // Big file size protection 
-	size_t bytesRead = 0;
-	memset(buffer, 0, sizeof(buffer));
-	
-	file = fopen(name, "rb");
-	if (file != NULL) {
-		while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-			printf("%s", buffer);
-			memset(buffer, 0, sizeof(buffer));
-		}
-	}
+void cat(char * name) {
+        FILE *file;
+        unsigned char buffer[1024]; // Big file size protection 
+        size_t bytesRead = 0;
+        memset(buffer, 0, sizeof(buffer));
+
+        file = fopen(name, "rb");
+        if (file != NULL) {
+                while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) { // fix trash in output
+                        printf("%d - %s", bytesRead, buffer);
+                        memset(buffer, 0, sizeof(buffer));
+                }
+        }
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2 || argc > 254) {
-		printf("Usage: cat [file1] [file2] ...");
-		return 0;
-	}
-	for(uint8_t i = 1; i < argc; i++) {
-		cat(argv[i]);
-	}
-	return 0;
+        if (argc < 2 || argc > 254) {
+                printf("Usage: cat [file1] [file2] ...");
+                return 0;
+        }
+        for(uint8_t i = 1; i < argc; i++) {
+                cat(argv[i]);
+        }
+        return 0;
 }
